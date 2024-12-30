792a5b6eefdb90548a4cc1272ab2df039b396e8d
Deyvid0809 <davi25090@gmail.com>
2024-12-30 07:41:58 -0300

    Criação da class título que tera todos os atributos em comum entre série e filme para assim poder ser usado como herança.Criação da class série e filme aonde tera o seus atributos próprios.

---
+++ src/modelos/Serie.java
@@ -0,0 +1,44 @@
+package modelos;
+
+public class Serie extends Titulo {
+    
+    private int temporadas;
+    private int episodiosPorTemporada;
+    private boolean ativa;
+    private int minutosPorEpisódio;
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisódio() {
+        return minutosPorEpisódio;
+    }
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+    
+    public boolean getAtiva(){
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public void setMinutosPorEpisódio(int minutosPorEpisódio) {
+        this.minutosPorEpisódio = minutosPorEpisódio;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    
+
+}
